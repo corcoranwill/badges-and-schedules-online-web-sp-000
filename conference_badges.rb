d90a031a75ae5ceaf6f522337619a25cbdbf8aca
Will Corcoran <corcoranwill@gmail.com>
2020-02-15 17:06:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,13 +5,11 @@
 
 def badge_maker(name)
   return "Hello, my name is #{name}"
-
-  # return a formatted badge "Hello, my name is #{name}"
 end
 
 def batch_badge_creator(names)
   badge_messages = []
-  
+
   names.each do |name|
     badge_messages = badge_maker(name)
   end
@@ -25,7 +23,6 @@ end
 def printer()
   # should first output the result of batch_badge_creator
   # output assign_rooms
-  # then output the badges and room assignments 
+  # then output the badges and room assignments
   # make sure you are iterating through your badges and room assignment lists
 end
-
